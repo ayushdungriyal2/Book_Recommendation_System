e0ea75ab5c1d0d0460d66bebb5a0050664b08ba8
Ayush Dungriyal <ayushdungriyal2@gmail.com>
2022-10-19 15:49:03 +0530

    dfdfdddd

--- recommendation/settings.py
+++ recommendation/settings.py
@@ -118,15 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "main/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "main/static")
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
-
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
